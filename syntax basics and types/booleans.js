9510a0172423668a669bebbb4a82114319e3b45f
charvi03 <charvikhurana01@gmail.com>
2023-08-07 11:42:15 +0530

    init

---
+++ syntax basics and types/booleans.js
@@ -0,0 +1,28 @@
+//booleans and comparisons
+console.log(true, false, "true", "false");
+
+//methods can return booleans
+let email = "ck@gmail.com";
+let names = ["ck", "nk", "ak"];
+// //let result = email.includes("!");
+// let result = names.includes("ck");
+// console.log(result);
+
+//comparison operators
+let age = 22;
+console.log(age === 22);
+console.log(age === "22"); //string comparison ===
+// console.log(age == 25);
+// console.log(age == 22);
+// console.log(age == "22");
+// console.log(age != "22");
+// console.log(age != 22);
+// console.log(age > 20);
+// console.log(age <= 25);
+// console.log(age >= 22);
+
+// let na = "hello";
+// console.log(na == "hello");
+// console.log(na == "Hello");
+// console.log(na > "world");
+// console.log(na > "Hello");
