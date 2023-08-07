f24cafd8837eca3aab1851bc3bca4b05e2978ceb
charvi03 <charvikhurana01@gmail.com>
2023-08-07 11:17:03 +0530

    init

---
+++ syntax basics and types/arrays.js
@@ -0,0 +1,18 @@
+let n = ["ck", "ak", "rk"];
+// n[1] = "nk";
+// console.log(n[1]);
+
+let ages = [20, 25, 30, 22];
+console.log(ages[2]);
+
+let random = ["ck", "nk", 10, 22];
+console.log(random);
+
+console.log(random.length);
+
+//let result = n.join(",,");
+//let result = n.indexOf("ak");
+//let result = n.concat(["abc", "pqr"]);
+let result = n.push("kk");
+result = n.pop();
+console.log(result);
