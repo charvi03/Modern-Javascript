dfafd5847adebc7f01d02d409e327ba3fc608ed9
charvi03 <charvikhurana01@gmail.com>
2023-08-23 14:05:46 +0530

    init

---
+++ syntax basics and types/Control_flow/loop.js
@@ -0,0 +1,19 @@
+// for (let i = 0; i < 5; i++) {
+//   console.log("IN LOOP ", i);
+// }
+
+// console.log("loop finished");
+
+// const names = ["abc", "xyz", "pqr"];
+// for (let i = 0; i < names.length; i++) {
+//   //console.log(i);
+//   // console.log(names[i]);
+//   let html = `<div>${names[i]}</div>`;
+//   console.log(html);
+// }
+let i = 0;
+
+while (i < 5) {
+  console.log("in loop ", i);
+  i++;
+}
